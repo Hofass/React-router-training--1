63299e31b7807a3b9ed55be8b79b3965b8721930
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-07-07 14:18:21 +0200

    Adding root layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,17 +24,24 @@ import HomePage from "./pages/HomePage";
 import EventsPage from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
-import EditEventPage from "./pages/EditEventPage"
+import EditEventPage from "./pages/EditEventPage";
+import Root from "./pages/Root";
 
-
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> },
-{path:"/events",element:<EventsPage/>},
-{path:"/events/:eventId", element:<EventDetailPage/>},
-{path:"/events/new", element:<NewEventPage/>},
-{path:"./events/eventId/edit", element:<EditEventPage/>}
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		children: [
+			{ index:true, element: <HomePage /> },
+			{ path: "events", element: <EventsPage /> },
+			{ path: "events/:eventId", element: <EventDetailPage /> },
+			{ path: "events/new", element: <NewEventPage /> },
+			{ path: "events/eventId/edit", element: <EditEventPage /> },
+		],
+	},
 ]);
 function App() {
-	return <RouterProvider router={router}></RouterProvider>
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
