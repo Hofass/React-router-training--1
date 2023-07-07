77bad08c7f1465c64b080fbc0717641a55497416
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-07-07 16:21:16 +0200

    Adding nested layout

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,6 +1,14 @@
+import { useParams } from "react-router-dom";
 
-function EventDetailPage(){
-  return <h1>Event Detail Page!</h1>
-  }
-  
-  export default EventDetailPage;
+function EventDetailPage() {
+	const params = useParams();
+
+	return (
+		<>
+			<h1>Event Detail Page!</h1>
+			<p>{params.eventId}</p>
+		</>
+	);
+}
+
+export default EventDetailPage;
